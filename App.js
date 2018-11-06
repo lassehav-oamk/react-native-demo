4b6781e8ba32d65209f9b98fd6c89471464f4365
Lasse Haverinen <lasse.haverinen@oamk.fi>
2018-11-06 13:56:37 +0200

    Added navigator and refactored initial code

--- App.js
+++ App.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, TextInput, ScrollView, KeyboardAvoidingView  } from 'react-native';
+import Login from './views/Login';
+import { createStackNavigator } from 'react-navigation';
+
+const RootStack = createStackNavigator({
+  Home: {
+    screen: Login
+  },
+});
 
 export default class App extends React.Component {
   render() {
-    return (  
-        <View style={styles.container}>            
-          <Image source={require("./assets/logo.png")} style={{ resizeMode: 'contain', height: 100, marginTop: 35 }}/>            
-          <View style={styles.loginInput}>
-            <Text>Username</Text>
-            <TextInput style={{paddingBottom: 5 }} textContentType={"username"}/>
-            <Text style={{ marginTop: 10}}>Password</Text>
-            <TextInput style={{marginBottom: 15, paddingBottom: 5}} textContentType={"password"}/>
-            <Button title="OK" onPress={(test) => {console.log("button clicked " + test)}}>Sign in</Button>
-          </View>
-                                      
-        </View>
-    );
+    return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {    
-    flex: 1,
-    backgroundColor: '#FAF',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    flexDirection: 'column',    
-    
-  },
-  loginInput: {    
-    justifyContent: 'center',
-    flexDirection: 'column',    
-    paddingTop: 15,
-    paddingBottom: 20,
-    width: 300
-  }
-  
-});
